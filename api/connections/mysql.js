e355dfb9ad13d221f2c8f2d64f7ba84c36f4180b
Indo Deriano <mde50526@gmail.com>
2020-05-30 16:26:17 +0800

    Update mysql.js

--- api/connections/mysql.js
+++ api/connections/mysql.js
@@ -25,7 +25,7 @@ db.connect((err)=>{
     if(err){
         console.log(err)
     }else{
-        console.log('ujian backend database is connected')
+        console.log('db4free is connected')
     }
 })
 
